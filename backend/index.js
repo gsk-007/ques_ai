b67557ce0309033abc94ef804223d752cb034871
gsk-007 <gsingh16742@gmail.com>
2025-04-27 13:32:20 +0530

    feat: add backend config and example env file

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./server.js";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 const start = () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI not present");
+  }
+
+  connectDB();
+
   app.listen(PORT, () => {
     console.log(`hello on http://localhost:${PORT}`);
   });
